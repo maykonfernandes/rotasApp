90fb98bcb1f26686ba0aeb7caa33b1977312c5fe
Maykon Fernandes <125682547+maykonfernandes@users.noreply.github.com>
2024-02-24 13:43:35 -0300

    up

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
+
+//IMPORT STACK NAVIGATION
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//CREATE A STACK NAVIGATOR INSTANCE
+// it allow us to use comands to define app navigation
+const Stack = createNativeStackNavigator;
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Sobre" component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Home() {
+  const navigation = useNavigation();
+
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello! This is my new project</Text>
+      <Button title="Sobre" onPress={() => navigation.navigate ("Sobre")} />
+    </View>
+  );
+}
+
+function Sobre() {
+  return (
+    <View style={styles.container}>
+      <Text>Sobre</Text>
     </View>
   );
 }
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
